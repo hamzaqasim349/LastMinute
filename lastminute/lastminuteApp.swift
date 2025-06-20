e5866ef42e6c62134a2bd849bbcba4054a2d02cd
Jabran <Jabran.ali1999@gmail.com>
2025-06-20 01:43:37 +0100

    Initial Commit

---
+++ lastminute/lastminuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  lastminuteApp.swift
+//  lastminute
+//
+//  Created by Jabran Ali on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct lastminuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lastminuteTests/lastminuteTests.swift
@@ -0,0 +1,17 @@
+//
+//  lastminuteTests.swift
+//  lastminuteTests
+//
+//  Created by Jabran Ali on 20/06/2025.
+//
+
+import Testing
+@testable import lastminute
+
+struct lastminuteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
